c32a9c56a9d8f9192ea0f4aaa3e725c580216787
Hemanshu <Hemanshu>
2017-04-14 19:49:21 +0530

    first commit

---
+++ modelid.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'modeid.json',
+        success: function(html) {
+            $('#child_selection').html(html);
+            $.each(html, function(key, value) {
+                $("#parent_selection").append("<option value=" + value.id + ">" + value.modeID + "</option>");
+            });
+
+            $('#parent_selection').on('change', function() {
+
+                var modelID = $(this).val();
+                if (modelID) {
+                    $.ajax({
+                        url: 'scnarion.json',
+                        success: function(data) {
+                            $("#child_selection").empty();
+                            $.each(data, function(key, value) {
+                                if(value.id == modelID){   
+                                  $.each(value.text, function(k,v){
+                                   $("#child_selection").append("<option value=" + v.id + ">" + v.senarioName + "</option>");
+                                  });                                                              
+                                }
+
+                            });
+
+
+                        }
+                    });
+                }
+            })
+
+        }
+
+
+    });
+
+});
